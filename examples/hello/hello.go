41695c2741cb19d2fedd03cb5fc900af4bc5e7f1
thedmpd <dmpd88@gmail.com>
2017-03-19 22:58:51 -0700

    deleted hello.go and new examples in code testing

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -4,5 +4,12 @@ import "fmt"
 
 func main(){
 	fmt.Printf("hello, world.\n")
+	fmt.Println("Hello Word"[1])
+	fmt.Printf("hi there Diogo\n")
+	fmt.Println(42)
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	for i := 0; i < 1000; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+	}
 }
 
